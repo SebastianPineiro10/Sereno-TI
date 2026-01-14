eab1b251e377571cb0392d311966db53709daeb3
SebastianPineiro10 <sebastian.bienesraices10@gmail.com>
2026-01-14 16:21:25 -0700

    Nuevos cambios

---
+++ src/data/servicesData.js
@@ -0,0 +1,43 @@
+export const servicesData = [
+  {
+    id: "web-dev",
+    title: "Desarrollo Web",
+    description:
+      "Páginas corporativas, paneles administrativos ,  e-commerce y  sistemas inteligentes conectados a PostgreSQL, Firebase o MongoDB.",
+    images: [
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768412533/NDEyfHdwLWNvbnRlbnQvdXBsb2Fkcy8yMDE4LzA4L2xvZ29Qb3N0Z3JlU1FMLnBuZw-Photoroom_llktuj.png",
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768412534/logo-vertical-Photoroom_if7d01.png",
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768412788/kuyjf3vea2hg34taa-horizontal_default_slate_blue_ukaott.png"
+    ]
+  },
+  {
+    id: "mobile-apps",
+    title: "Aplicaciones Móviles",
+    description:
+      "Apps modernas con Expo. Desde app modernas hasta productos listos para escalar.",
+    images: [
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768415707/expos-Photoroom_opykag.png",
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768416425/0501c7cb-74eb-4c0d-bc6f-4634bd0bf709-Photoroom_kh3key.png"
+    ]
+  },
+  {
+    id: "tech-consulting",
+    title: "Consultoría Técnica",
+    description:
+      "Te ayudamos a definir tu arquitectura digital, resolver problemas técnicos o escalar tu infraestructura existente.",
+    images: [
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768416277/maquetacio%CC%81n_1_-Photoroom_zuzrm9.png"
+    ]
+  },
+  {
+    id: "integration",
+    title: "Integración de Plataformas",
+    description:
+      "Conectamos tus sistemas con APIs, CRMs, ERPs u otras soluciones personalizadas para aumentar tu eficiencia operativa.",
+    images: [
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768416821/api-Photoroom_mo2lh6.png",
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768417821/crm_tbtb78.png",
+      "https://res.cloudinary.com/dcerhiol0/image/upload/v1768418067/erp-Photoroom_sere3s.png"
+    ]
+  }
+];
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,5 @@
-// src/pages/Services.jsx
 import "./Services.css";
+import { servicesData } from "../data/servicesData";
 
 export default function Services() {
   return (
@@ -10,30 +10,20 @@ export default function Services() {
       </p>
 
       <div className="services-grid">
-        <div className="service-box">
-          <h3>Desarrollo Web</h3>
-          <p>
-            Páginas corporativas, paneles administrativos y sistemas inteligentes conectados a bases de datos como PostgreSQL, Firebase o MongoDB.
-          </p>
-        </div>
-        <div className="service-box">
-          <h3>Aplicaciones Móviles</h3>
-          <p>
-            Apps modernas con Expo y React Native. Desarrollamos desde MVPs hasta productos listos para escalar.
-          </p>
-        </div>
-        <div className="service-box">
-          <h3>Consultoría Técnica</h3>
-          <p>
-            Te ayudamos a definir tu arquitectura digital, resolver problemas técnicos o escalar tu infraestructura existente.
-          </p>
-        </div>
-        <div className="service-box">
-          <h3>Integración de Plataformas</h3>
-          <p>
-            Conectamos tus sistemas con APIs, CRMs, ERPs o soluciones personalizadas para aumentar tu eficiencia operativa.
-          </p>
-        </div>
+        {servicesData.map(({ id, title, description, images }) => (
+          <div key={id} className="service-box">
+            <div className="service-content">
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+
+            <div className="service-icons">
+              {images.map((img, index) => (
+                <img key={index} src={img} alt={`Icono ${index + 1}`} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
